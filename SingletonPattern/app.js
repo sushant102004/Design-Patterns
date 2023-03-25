5a095b6613db22ad255b489e27704fc5d9643243
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-25 17:35:03 +0530

    🐛 fix(app.js): fix getProcessManager function to return processManager when it's already defined

---
+++ SingletonPattern/app.js
@@ -0,0 +1,29 @@
+const SingletonDesignPattern = (function() {
+    let processManager
+
+    class ProcessManager {
+        constructor() {
+            this.numberOfProcesses = 0
+        }
+    }
+
+    function createProcessManager() {
+        console.log('Creating Process Manager')
+        processManager = new ProcessManager()
+        return processManager
+    }
+
+    return {    
+        getProcessManager: () => {
+            if(processManager === undefined) {
+               console.log('Inside Get Process Manager')
+               processManager = createProcessManager()
+               return processManager 
+            }
+        }
+    }
+})()
+
+const pManagerOne = SingletonDesignPattern.getProcessManager()
+const pManagerTwo = SingletonDesignPattern.getProcessManager()
+const pManagerThree = SingletonDesignPattern.getProcessManager()
