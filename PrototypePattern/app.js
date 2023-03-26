c3e4d49374ffe727a816f489919e4812a9ee209e
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-26 16:13:30 +0530

    ✨ feat(app.js): add implementation of Prototype Pattern for creating Car objects

---
+++ PrototypePattern/app.js
@@ -0,0 +1,45 @@
+class Car {
+    constructor(name, model, wheels) {
+        this.name = name
+        this.model = model
+        this.wheels = wheels
+    }
+    start() { console.log('Engine Started') }
+}
+
+class CarPrototype {
+    constructor(proto) {
+        this.clone = () => {
+            const newCar = new Car(proto.name, proto.model, proto.wheels)
+            return newCar
+        }
+    }
+}
+
+const carHandler = (function() {
+    return {
+        createMercedes2022: () => {
+            const mercedes2022Proto = new Car('Mercedes Benz 22', 2022, 4)
+            const prototype = new CarPrototype(mercedes2022Proto)
+            return prototype.clone()
+        },
+        createBharatBenzTruck: () => {
+            const bharaBenzTruckProto = new Car('Bharat Benz', 2010, 8)
+            const prototype = new CarPrototype(bharaBenzTruckProto)
+            return prototype.clone()
+        }
+    }
+})()
+
+const mercedes = carHandler.createMercedes2022()
+const bharatBenz = carHandler.createBharatBenzTruck()
+
+
+console.log(mercedes)
+console.log(bharatBenz)
+
+// Adding more object values
+
+mercedes.topSpeed = '318 Km/h'
+
+console.log(mercedes)
